c51adf41a5577b1aa94ead6e1df08d4633355cf0
baiest <ballesteros.juan@correounivalle.edu.co>
2021-03-31 17:11:49 -0500

    Iniciado movimiento de la serpiente

--- src/model/state.rb
+++ src/model/state.rb
@@ -1,6 +1,12 @@
 # Representacion del estado del juego
 
 module Model
+    module Direction
+        UP = :up
+        RIGHT = :right
+        DOWN = :down
+        LEFT = :left
+    end
     class Coord < Struct.new(:row, :col)
     end
     
@@ -13,7 +19,7 @@ module Model
     class Grid < Struct.new(:rows, :cols)
     end
     
-    class State < Struct.new(:snake, :food, :grid)
+    class State < Struct.new(:snake, :food, :grid, :next_direction)
     end
 
     def self.initial_state
@@ -24,6 +30,7 @@ module Model
             ]),
             Model::Food.new(4,4),
             Model::Grid.new(8, 12),
+            Direction::DOWN
         )
     end
 end
