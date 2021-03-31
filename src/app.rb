b717f582739ee94abe534c913a37ed1a369aebc4
baiest <ballesteros.juan@correounivalle.edu.co>
2021-03-31 16:48:23 -0500

    Estado inicial del juego agregado

--- src/app.rb
+++ src/app.rb
@@ -1,3 +1,7 @@
-require 'ruby2d'
+require_relative "view/ruby2d"
+require_relative "model/state"
 
-show
+view = View::Ruby2dView.new
+
+initial_state = Model::initial_state
+view.render(initial_state)
